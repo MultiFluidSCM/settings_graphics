42b5371996c0a260464b5b8ab17aaf6205b107db
statisdisc <will.a.m@hotmail.co.uk>
2021-09-02 10:08:27 +0100

    Converted scm comparison script to new standard.

--- make_scm_settings_graphic.py
+++ make_scm_settings_graphic.py
@@ -19,19 +19,24 @@ from src.utilities.read_scm import read_transfer_properties
 from src.utilities.time_elapsed import time_elapsed
 
 @time_elapsed
-def make_graphics(id_scm="default", greyscale=False):
+def make_scm_graphic(id_scm="default", greyscale=False):
+    '''
+    Read in the tunable parameters for the single column model (SCM) version and display them
+    relative to what the values should be according to diagnosed high-resolution data (LES).
     
+    :param id_scm: The folder name/id containing the SCM settings files, str.
+    :param greyscale: Select whether the graphics should be in color or grey, bool.
+    :return: Dictionary of SCM settings and LES data, dict dict.
+    '''
     print(f"\n\nCreating graphics for {id_scm}")
-    
     folder = path_setup(__file__)
-    
     folder_scm_settings = os.path.join(folder.data_scm, id_scm, "settings", "transfer_properties")
     
     print("\nReading data")
     les_data = read_les(folder.data_les)
     scm_settings = read_transfer_properties(folder_scm_settings)
     
-    print("\nProcessing data")
+    print("\nProcessing, cleaning and preparing data")
     scm_settings_transfers = filter_transfer_coefficients(scm_settings)
     les_data_b = diagnose_and_filter_b(les_data)
     
@@ -39,10 +44,12 @@ def make_graphics(id_scm="default", greyscale=False):
     plot_all_transfer_graphics(
         scm_settings_transfers, 
         les_data_b, 
-        folder = os.path.join(folder.outputs, id_scm), 
-        title = id_scm,
+        folder    = os.path.join(folder.outputs, id_scm), 
+        title     = id_scm,
         greyscale = greyscale
     )
+    
+    return scm_settings_transfers, les_data_b
     
     
     
@@ -50,5 +57,5 @@ def make_graphics(id_scm="default", greyscale=False):
 if __name__ == "__main__":
     id_scm = "default_simulation_version_2"
     
-    make_graphics(id_scm=id_scm, greyscale=True)
-    # make_graphics(id_scm=id_scm, greyscale=False)
+    make_scm_graphic(id_scm=id_scm, greyscale=True)
+    make_scm_graphic(id_scm=id_scm, greyscale=False)
